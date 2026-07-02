02e579c6b8cb1ff633955acfe54dff51322b3d39
leaugueofchoding <hispkty@gmail.com>
2026-07-02 11:27:55 +0900

    feat: register ice bear pet patch

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './features/pet/iceBearPatch.js'
 import App from './App.jsx'
 import GlobalStyle from './styles/GlobalStyle.js'
 
