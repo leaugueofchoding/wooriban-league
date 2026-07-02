1312bbbb5f2733398f64870fefc5f2e72ece996e
leaugueofchoding <hispkty@gmail.com>
2026-07-02 12:40:00 +0900

    revert: unregister ice bear patch module

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './features/pet/iceBearPatch.js'
 import App from './App.jsx'
 import GlobalStyle from './styles/GlobalStyle.js'
 
